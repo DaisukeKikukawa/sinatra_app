d1c1010944f5663e861b4cfe9a77e15b7f6fbc50
Daisuke <a1485@icloud.com>
2021-10-06 19:32:59 +0900

    fix:不要部分の削除

--- app.rb
+++ app.rb
@@ -9,15 +9,8 @@ require 'pg'
 enable :method_override
 
 get '/memos' do
-  # memo_files = Dir.glob("./db/*")
-  # @memo_files_array = memo_files.map {|files| JSON.parse(File.open(files).read, symbolize_names: true)}
-
-  # conn = PG.connect(dbname: 'memos')
-  # @memos = conn.exec('SELECT * FROM memos_app;')
-
   conn = PG.connect(dbname: 'memos')
   @memos = conn.exec("SELECT * FROM memos_app")
-
   erb :index
 end
 
@@ -33,11 +26,6 @@ end
 
 get '/memos/:id' do |n|
   @edit = "/memos/#{params['id']}/edit"
-  # file = File.open("./db/#{n}.json", "r")
-  # memo = JSON.load(file)
-  # @title = memo["title"]
-  # @content = memo["content"]
-
   conn = PG.connect(dbname: 'memos')
   @memos = conn.exec('SELECT * FROM memos_app WHERE id = $1;', [params['id']])
 
@@ -45,8 +33,6 @@ get '/memos/:id' do |n|
 end
 
 delete '/memos/:id' do
-  # File.delete("./db/#{params['id']}.json")
-  # redirect "/memos"
   conn = PG.connect(dbname: 'memos')
   conn.exec('DELETE FROM memos_app WHERE id = $1;', [params['id']])
 
@@ -55,35 +41,15 @@ end
 
 get '/memos/:id/edit' do |n|
   @id = "/memos/#{params['id']}"
-  # file = File.open("./db/#{n}.json", "r")
-  # memo = JSON.load(file)
-  # @title = memo["title"]
-  # @content = memo["content"]
-
   conn = PG.connect(dbname: 'memos')
   @memos = conn.exec('SELECT * FROM memos_app WHERE id = $1;', [params['id']] )
   erb :edit
 end
 
 post '/memos' do
-  # p "post!!!!!"
-
-  # binding.irb
-
   @id = SecureRandom.uuid
   @title = params[:title]
   @content = params[:content]
-  # memo_id = SecureRandom.alphanumeric
-  # memos = {"memo_id"=>"#{memo_id}","title"=>@title, "content"=>@content}
-  # File.open("./db/#{memo_id}.json", 'w') do |file|
-  #   JSON.dump(memos, file)
-  # end
-  # memo_files = Dir.glob("./db/*")
-  # @memo_files_array = memo_files.map {|files| JSON.parse(File.open(files).read, symbolize_names: true)}
-
-  # conn = PG.connect( dbname: 'memos')
-  # conn.exec("INSERT INTO memos_app (id, title, content) VALUES ('#{@id}', '#{@title}', '#{@content}')")
-
   conn = PG.connect(dbname: 'memos')
   conn.exec("INSERT INTO memos_app (title, content) VALUES ( '#{params['title']}', '#{params['content']}');")
 
@@ -91,11 +57,6 @@ post '/memos' do
 end
 
 patch '/memos/:id' do
-  # File.open("./db/#{params['id']}.json",'w') do |file|
-  #   hash = {id: "#{params['id']}",title: params[:title],body: params[:body]}
-  #   JSON.dump(hash,file)
-  # end
-
   conn = PG.connect(dbname: 'memos')
   conn.exec('UPDATE memos_app SET title = $1, content = $2 WHERE id = $3;', [params['title'], params['content'], params['id']])
   redirect to("/memos")
